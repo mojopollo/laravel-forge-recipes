6eebdcac7613d716e2c93a9958c578c60b0bf873
Jaime Medina <jmedina@qujo.com>
2016-05-14 20:46:16 -0700

    adds redis-server to disable - reorders run order

--- system-stop-and-disable-services-on-boot.sh
+++ system-stop-and-disable-services-on-boot.sh
@@ -1,10 +1,14 @@
+# Stop services installed by forge
+service mysql stop
+service postgresql stop
+service beanstalkd stop
+service redis-server stop
+service memcached stop
+
+# Disable services installed by forge
+# from starting at boot time
 systemctl disable mysql.service
 systemctl disable postgresql.service
 systemctl disable beanstalkd.service
 systemctl disable redis-server.service
 systemctl disable memcached.service
-
-service mysql stop
-service postgresql stop
-service beanstalkd stop
-service memcached stop
